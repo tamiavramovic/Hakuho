bf51f7c021a1370e56a5af0e891615c4d9b1cf92
you@example.com <you@example.com>
2016-11-29 08:28:05 +0200

    connected to ip cam

---
+++ Docs/CMakeFiles/Video.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/Video.dir/video.cpp.o"
+  "Video.pdb"
+  "Video"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/Video.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
--- Docs/video.cpp
+++ Docs/video.cpp
@@ -197,8 +197,7 @@ int main(int argc, char* argv[])
 	//video capture object to acquire webcam feed
 	VideoCapture capture;
 	//open capture object at location zero (default location for webcam)
-	capture.open(0);
-	//set height and width of capture frame
+	capture.open("rtmp://172.16.254.63/live/live");	//set height and width of capture frame
 	capture.set(CV_CAP_PROP_FRAME_WIDTH, FRAME_WIDTH);
 	capture.set(CV_CAP_PROP_FRAME_HEIGHT, FRAME_HEIGHT);
 	//start an infinite loop where webcam feed is copied to cameraFeed matrix
